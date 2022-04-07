5f0d069fd87d7e121238e4ad12a7ed8fa7690482
ermek-koktalov <95706888+ermek-koktalov@users.noreply.github.com>
2022-04-07 15:25:42 +0200

    1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+import Futter from "./Futter/Futter";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Futter />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Futter/Futter.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Futter() {
+  return <div>Futter</div>;
+}
+
+export default Futter;
